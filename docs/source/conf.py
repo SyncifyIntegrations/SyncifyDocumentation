389ccd5393ae496eeb57608d9ae01e50c36cc8b2
Syncify AB <kundservice@syncify.se>
2024-02-20 11:14:12 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Syncify Documentation'
+copyright = '2024, Syncify AB'
+author = 'Syncify AB'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_material'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
